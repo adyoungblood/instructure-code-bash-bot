df59a13d73550cc275b0c7ef495aefca6b0a0ab5
Alexander Youngblood <alexander.youngblood@instructure.com>
2023-07-28 13:54:22 -0700

    Pretty sure this one won't be silly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import copy
 
 base_url = 'http://light-bikes.inseng.net/games'
 timeout = 3
@@ -38,27 +39,44 @@ sight = [
     [-1, 0]
 ]
 
-def get_next_position(current_x, current_y, board, depth = 2):
-    if depth == 0:
+def get_next_position(current_x, current_y, board, max_depth = 2, depth = 0):
+    if depth > max_depth:
         return
-    
-    next_position = (current_x, current_y)
+
+    potential_next_positions = []
     for offset in sight:
         new_x = current_x + offset[0]
         new_y = current_y + offset[1]
+        # stuff = (len(board), len(board[0]), board[new_x][new_y])
         if new_x >= 0 and new_y >= 0 and new_x < len(board) and new_y < len(board[0]) and board[new_x][new_y] == None:
-            board[new_x][new_y] = 'bleh'
-            next_next_position = get_next_position(new_x, new_y, board, depth - 1)
-            if next_next_position is not None and next_next_position == (new_x, new_y):
-                board[new_x][new_y] = None
-                break
-            board[new_x][new_y] = None
-            next_position = (new_x, new_y)
-    return next_position
+            simulated_board = copy.deepcopy(board)
+            simulated_board[new_x][new_y] = 'bleh'
+            next_next_position = get_next_position(new_x, new_y, simulated_board, max_depth, depth + 1)
+            if next_next_position != (new_x, new_y):
+                potential_next_positions.append((new_x, new_y))
+
+    if len(potential_next_positions) == 0:
+        return (current_x, current_y)
+
+    potential_next_position = potential_next_positions[0]
+    potential_next_sticky_positions = []
+    if depth == 0:
+        for position in potential_next_positions:
+            can_stick = False
+            for offset in sight:
+                new_x = position[0] + offset[0]
+                new_y = position[1] + offset[1]
+                if new_x >= 0 and new_y >= 0 and new_x < len(board) and new_y < len(board[0]) and board[new_x][new_y] is not None:
+                    can_stick = True
+                    break
+            if can_stick:
+                potential_next_sticky_positions.append(position)
+        potential_next_sticky_positions
+    return potential_next_sticky_positions[0] if len(potential_next_sticky_positions) > 0 else potential_next_position
 
 # -----------------
 
-def main():
+def main(max_depth):
     should_create = True if input('Create a new game? (y)/n') == '' else False
     if should_create: 
         game_id = create(add_server_bot = True, server_bot_difficulty = 4).json()['id']
@@ -75,7 +93,7 @@ def main():
         current_x = game_state['current_player']['x']
         current_y = game_state['current_player']['y']
         board = game_state['board']
-        next_move = get_next_position(current_x, current_y, board)
+        next_move = get_next_position(current_x, current_y, board, max_depth)
         response = move(game_id, player_id, next_move[0], next_move[1])
         status_code = response.status_code
         # print(response.status_code, response.json())
@@ -84,14 +102,16 @@ def main():
         print('You won!')
     else:
         print('You lost...')
-def test():
+def test(max_depth):
     test_board = [
-        [1, None, None],
-        [None, 1, None],
-        [1, 1, 1]
+        [1, 1, None, None, None],
+        [1, None, None, None, None],
+        [None, None, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1]
     ]
-    print(get_next_position(1, 1, test_board))
+    print(get_next_position(2, 2, test_board, max_depth))
 
 if __name__ == '__main__':
-    main()
+    main(2)
     
